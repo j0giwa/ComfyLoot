9911567771f813a034e22ef1a6b9d9b8de09bc19
j0giwa <77389555+j0giwa@users.noreply.github.com>
2025-09-03 17:48:38 +0200

    Fix Formating

--- ComfyLoot/Configuration.cs
+++ ComfyLoot/Configuration.cs
@@ -6,14 +6,15 @@ namespace ComfyLoot;
 [Serializable]
 public class Configuration : IPluginConfiguration
 {
-    public int Version { get; set; } = 0;
+	public int Version { get; set; } = 0;
 
-    public bool IsConfigWindowMovable { get; set; } = true;
-    public bool SomePropertyToBeSavedAndWithADefault { get; set; } = true;
+	public bool IsConfigWindowMovable { get; set; } = true;
+	public bool SomePropertyToBeSavedAndWithADefault { get; set; } = true;
 
-    // The below exist just to make saving less cumbersome
-    public void Save()
-    {
-        Plugin.PluginInterface.SavePluginConfig(this);
-    }
+	// The below exist just to make saving less cumbersome
+	public void
+	Save()
+	{
+		Plugin.PluginInterface.SavePluginConfig(this);
+	}
 }
--- ComfyLoot/Plugin.cs
+++ ComfyLoot/Plugin.cs
@@ -10,72 +10,78 @@ namespace ComfyLoot;
 
 public sealed class Plugin : IDalamudPlugin
 {
-    [PluginService] internal static IDalamudPluginInterface PluginInterface { get; private set; } = null!;
-    [PluginService] internal static ITextureProvider TextureProvider { get; private set; } = null!;
-    [PluginService] internal static ICommandManager CommandManager { get; private set; } = null!;
-    [PluginService] internal static IClientState ClientState { get; private set; } = null!;
-    [PluginService] internal static IDataManager DataManager { get; private set; } = null!;
-    [PluginService] internal static IPluginLog Log { get; private set; } = null!;
+	[PluginService]
+	internal static IDalamudPluginInterface PluginInterface { get; private set; } = null!;
+	[PluginService]
+	internal static ITextureProvider TextureProvider { get; private set; } = null!;
+	[PluginService]
+	internal static ICommandManager CommandManager { get; private set; } = null!;
+	[PluginService]
+	internal static IClientState ClientState { get; private set; } = null!;
+	[PluginService]
+	internal static IDataManager DataManager { get; private set; } = null!;
+	[PluginService]
+	internal static IPluginLog Log { get; private set; } = null!;
 
-    private const string CommandName = "/pmycommand";
+	private const string CommandName = "/loot";
 
-    public Configuration Configuration { get; init; }
+	public Configuration Configuration { get; init; }
+	public readonly WindowSystem WindowSystem = new("ComfyLoot");
+	private ConfigWindow ConfigWindow { get; init; }
+	private MainWindow MainWindow { get; init; }
 
-    public readonly WindowSystem WindowSystem = new("SamplePlugin");
-    private ConfigWindow ConfigWindow { get; init; }
-    private MainWindow MainWindow { get; init; }
+	public Plugin()
+	{
+		Configuration = PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
 
-    public Plugin()
-    {
-        Configuration = PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
+		// You might normally want to embed resources and load them from the manifest stream
+		var goatImagePath = Path.Combine(PluginInterface.AssemblyLocation.Directory?.FullName!, "goat.png");
 
-        // You might normally want to embed resources and load them from the manifest stream
-        var goatImagePath = Path.Combine(PluginInterface.AssemblyLocation.Directory?.FullName!, "goat.png");
+		ConfigWindow = new ConfigWindow(this);
+		MainWindow = new MainWindow(this, goatImagePath);
 
-        ConfigWindow = new ConfigWindow(this);
-        MainWindow = new MainWindow(this, goatImagePath);
+		WindowSystem.AddWindow(ConfigWindow);
+		WindowSystem.AddWindow(MainWindow);
 
-        WindowSystem.AddWindow(ConfigWindow);
-        WindowSystem.AddWindow(MainWindow);
+		CommandManager.AddHandler(CommandName, new CommandInfo(OnCommand) {
+			HelpMessage = "A useful message to display in /xlhelp"
+		});
 
-        CommandManager.AddHandler(CommandName, new CommandInfo(OnCommand)
-        {
-            HelpMessage = "A useful message to display in /xlhelp"
-        });
+		PluginInterface.UiBuilder.Draw += DrawUI;
 
-        PluginInterface.UiBuilder.Draw += DrawUI;
+		// This adds a button to the plugin installer entry of this plugin which allows
+		// toggling the display status of the configuration ui
+		PluginInterface.UiBuilder.OpenConfigUi += ToggleConfigUI;
 
-        // This adds a button to the plugin installer entry of this plugin which allows
-        // toggling the display status of the configuration ui
-        PluginInterface.UiBuilder.OpenConfigUi += ToggleConfigUI;
+		// Adds another button doing the same but for the main ui of the plugin
+		PluginInterface.UiBuilder.OpenMainUi += ToggleMainUI;
 
-        // Adds another button doing the same but for the main ui of the plugin
-        PluginInterface.UiBuilder.OpenMainUi += ToggleMainUI;
+		// Add a simple message to the log with level set to information
+		// Use /xllog to open the log window in-game
+		// Example Output: 00:57:54.959 | INF | [SamplePlugin] ===A cool log message from Sample Plugin===
+		Log.Information($"===A cool log message from {PluginInterface.Manifest.Name}===");
+	}
 
-        // Add a simple message to the log with level set to information
-        // Use /xllog to open the log window in-game
-        // Example Output: 00:57:54.959 | INF | [SamplePlugin] ===A cool log message from Sample Plugin===
-        Log.Information($"===A cool log message from {PluginInterface.Manifest.Name}===");
-    }
+	public void
+	Dispose()
+	{
+		WindowSystem.RemoveAllWindows();
 
-    public void Dispose()
-    {
-        WindowSystem.RemoveAllWindows();
+		ConfigWindow.Dispose();
+		MainWindow.Dispose();
 
-        ConfigWindow.Dispose();
-        MainWindow.Dispose();
+		CommandManager.RemoveHandler(CommandName);
+	}
 
-        CommandManager.RemoveHandler(CommandName);
-    }
+	private void
+	OnCommand(string command, string args)
+	{
+		// In response to the slash command, toggle the display status of our main ui
+		ToggleMainUI();
+	}
 
-    private void OnCommand(string command, string args)
-    {
-        // In response to the slash command, toggle the display status of our main ui
-        ToggleMainUI();
-    }
+	private void DrawUI() => WindowSystem.Draw();
 
-    private void DrawUI() => WindowSystem.Draw();
-
-    public void ToggleConfigUI() => ConfigWindow.Toggle();
-    public void ToggleMainUI() => MainWindow.Toggle();
-}
+	public void ToggleConfigUI() => ConfigWindow.Toggle();
+	public void ToggleMainUI() => MainWindow.Toggle();
+}	
