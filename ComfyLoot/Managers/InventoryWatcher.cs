43dacde8ffb2a642a66d388940fd4dd16eaec56a
j0giwa <77389555+j0giwa@users.noreply.github.com>
2025-10-23 11:31:24 +0200

    Fixed editorconfig to match style guide

--- ComfyLoot/Managers/InventoryWatcher.cs
+++ ComfyLoot/Managers/InventoryWatcher.cs
@@ -38,24 +38,23 @@ public class InventoryWatcher : IDisposable
 	private void
 	HandleAddItem(InventoryItemAddedArgs args)
 	{
-		switch (args.Inventory)
-		{
-			case GameInventoryType.Inventory1: /* FALLTHROUGH */
-			case GameInventoryType.Inventory2:
-			case GameInventoryType.Inventory3:
-			case GameInventoryType.Inventory4:
-			case GameInventoryType.Crystals:
-			case GameInventoryType.Currency:
-				_log.Information(
-					"[ADD] {Quantity}x {ItemId} in {Inventory} (slot {Slot})",
-					args.Item.Quantity,
-					args.Item.ItemId,
-					args.Inventory,
-					args.Slot);
-				_ = Task.Run(() => _loot.AddItem(args));
-				break;
-			default:
-				break;
+		switch (args.Inventory){
+		case GameInventoryType.Inventory1: /* FALLTHROUGH */
+		case GameInventoryType.Inventory2:
+		case GameInventoryType.Inventory3:
+		case GameInventoryType.Inventory4:
+		case GameInventoryType.Crystals:
+		case GameInventoryType.Currency:
+			_log.Information(
+				"[ADD] {Quantity}x {ItemId} in {Inventory} (slot {Slot})",
+				args.Item.Quantity,
+				args.Item.ItemId,
+				args.Inventory,
+				args.Slot);
+			_ = Task.Run(() => _loot.AddItem(args));
+			break;
+		default:
+			break;
 		}
 	}
 
@@ -69,36 +68,33 @@ public class InventoryWatcher : IDisposable
 		int addedAmount;
 		(uint ItemId, GameInventoryType Inventory, uint Slot) key;
 
-		switch (args.Inventory)
-		{
-			case GameInventoryType.Inventory1:
-			case GameInventoryType.Inventory2:
-			case GameInventoryType.Inventory3:
-			case GameInventoryType.Inventory4:
-			case GameInventoryType.Crystals:
-			case GameInventoryType.Currency:
-				previousQty = args.OldItemState.Quantity;
-				key = (args.Item.ItemId, args.Inventory, args.Slot);
-				/* First time seeing this item — treat as "baseline", not an actual change */
-				if (!_seenItems.Contains(key))
-				{
-					_seenItems.Add(key);
-					return; /* Ignore first update */
-				}
-				addedAmount = args.Item.Quantity - previousQty;
-				if (args.Item.Quantity > 0)
-				{
-					_log.Information(
-						"[CHANGE] {Quantity}x {ItemId} in {Inventory} (slot {Slot})",
-						addedAmount,
-						args.Item.ItemId,
-						args.Inventory,
-						args.Slot);
-					_loot.UpdateItem(args.Item.ItemId, addedAmount);
-				}
-				break;
-			default:
-				break;
+		switch (args.Inventory) {
+		case GameInventoryType.Inventory1:
+		case GameInventoryType.Inventory2:
+		case GameInventoryType.Inventory3:
+		case GameInventoryType.Inventory4:
+		case GameInventoryType.Crystals:
+		case GameInventoryType.Currency:
+			previousQty = args.OldItemState.Quantity;
+			key = (args.Item.ItemId, args.Inventory, args.Slot);
+			/* First time seeing this item — treat as "baseline", not an actual change */
+			if (!_seenItems.Contains(key)) {
+				_seenItems.Add(key);
+				return; /* Ignore first update */
+			}
+			addedAmount = args.Item.Quantity - previousQty;
+			if (args.Item.Quantity > 0) {
+				_log.Information(
+					"[CHANGE] {Quantity}x {ItemId} in {Inventory} (slot {Slot})",
+					addedAmount,
+					args.Item.ItemId,
+					args.Inventory,
+					args.Slot);
+				_loot.UpdateItem(args.Item.ItemId, addedAmount);
+			}
+			break;
+		default:
+			break;
 		}
 	}
 
@@ -109,16 +105,15 @@ public class InventoryWatcher : IDisposable
 	OnInventoryChanged(IReadOnlyCollection<InventoryEventArgs> events)
 	{
 		foreach (var evt in events)
-			switch (evt)
-			{
-				case InventoryItemAddedArgs added:
-					HandleAddItem(added);
-					break;
-				case InventoryItemChangedArgs changed:
-					HandleChangeItem(changed);
-					break;
-				default:
-					break;
+			switch (evt) {
+			case InventoryItemAddedArgs added:
+				HandleAddItem(added);
+				break;
+			case InventoryItemChangedArgs changed:
+				HandleChangeItem(changed);
+				break;
+			default:
+				break;
 			}
 	}
 
